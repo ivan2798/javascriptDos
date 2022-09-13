4434e6159372fbcc1cd057920543d5a4a646f127
ivan2798 <inge1998@hotmail.com>
2022-09-13 17:56:01 -0500

    objetos

---
+++ app.js
@@ -0,0 +1,64 @@
+//console.log("Estructura de datos");
+
+//ARRAYS
+//const no va a cambiar
+//let si, mejor que var
+let listaCompra = ["pan","leche","fruta","pescado"];
+
+let cantidades=[3,10,5,2];
+
+let registroEntradas = [true,true,true,false,false];
+
+//OBJETOS
+
+/*
+let cliente = {
+    nombre: "Ivan",
+    direccion: "C/ejemplo",
+    ciudad: "Market",
+    factura: 2300,
+    facturasPendientes: true
+}
+*/
+
+//COMBINACIONES
+
+//ARREGLO DE OBJETOS
+
+let arreglo = [{},{},{}];
+
+//camelcase
+const topTresJugadoresTenis = [{
+    ranking:1,
+    nombre: "Djokvic",
+    puntos:200,
+    pais: "Serbia",
+},{
+    ranking:2,
+    nombre: "Yakamura",
+    puntos:300,
+    pais: "Japon",
+},{
+    ranking:3,
+    nombre: "Pablo",
+    puntos:100,
+    pais: "Colombia",
+}]
+
+//objeto con arreglos
+
+let Djokovic = {
+    ranking : 1,
+    puntos: 300,
+    pais: "Serbia",
+    GrandSlams:17,
+    Activo:true,
+    estaTemporadaResults:{
+        wins:["Australian OPen"," Cincinatti","Rome","Dubai"],
+        finals:["Ronald Garros"],
+        quarterFinals: ["Vienna"]
+    },
+    nickName: ["Nole","Pinza"]
+}
+
+console.log(Djokovic)
